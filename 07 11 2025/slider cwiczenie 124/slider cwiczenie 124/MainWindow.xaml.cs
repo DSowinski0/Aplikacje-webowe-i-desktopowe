544bae80b8f25f277a160ef38b7d14580315c823
DSowinski0 <dawidsowinski2009@gmail.com>
2025-11-07 12:58:48 +0100

    Add files via upload

---
+++ 07 11 2025/slider cwiczenie 124/slider cwiczenie 124/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+
+
+
+
+
+
+// ma się zmieniać wartość
+// ma się zmieniać wielkość litery 0 - mała, 10 - duża
+
+
+
+
+namespace slider_cwiczenie_124
+{
+  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+      
+
+
+    }
+}
