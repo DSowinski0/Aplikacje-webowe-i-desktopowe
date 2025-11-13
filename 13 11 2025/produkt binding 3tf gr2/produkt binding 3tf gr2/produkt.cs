5af714f326b582e11f3f454dd061e06747362b4e
DSowinski0 <dawidsowinski2009@gmail.com>
2025-11-13 16:28:29 +0100

    Add files via upload

---
+++ 13 11 2025/produkt binding 3tf gr2/produkt binding 3tf gr2/produkt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace produkt_binding_3tf_gr2
+{
+    internal class produkt
+    {
+
+
+
+
+        public string Spadek { get; set; }
+
+        public string Nowosc { get; set; }
+
+        public string Ilosc_sztuk { get; set; }
+
+        public string Magazyn { get; set; }
+
+
+
+
+
+
+    }
+}
